95b8474f53971d116dfe11f01065b565c8db56b8
ciwei <1085160569@qq.com>
2019-11-30 15:58:51 +0800

    添加事务注解

--- src/main/java/com/mxc/springbootmybatisquick/service/BusinessLicenseService.java
+++ src/main/java/com/mxc/springbootmybatisquick/service/BusinessLicenseService.java
@@ -6,7 +6,7 @@ import com.mxc.springbootmybatisquick.mybatis.model.BusinessLicense;
 /**
  * @author maxiucheng
  * @className BusinessLicenseService
- * @description
+ * @description 测试接口方法
  * @date 2019/11/28 5:33 下午
  * @menu
  **/
--- src/main/java/com/mxc/springbootmybatisquick/service/impl/BusinessLicenseServiceImpl.java
+++ src/main/java/com/mxc/springbootmybatisquick/service/impl/BusinessLicenseServiceImpl.java
@@ -5,15 +5,17 @@ import com.mxc.springbootmybatisquick.mybatis.mapper.BusinessLicenseMapper;
 import com.mxc.springbootmybatisquick.mybatis.model.BusinessLicense;
 import com.mxc.springbootmybatisquick.service.BusinessLicenseService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author maxiucheng
  * @className BusinessLicenseServiceImpl
- * @description
+ * @description 测试接口实现
  * @date 2019/11/28 5:33 下午
  * @menu
  **/
 @Service
+@Transactional(rollbackFor = Exception.class)
 public class BusinessLicenseServiceImpl extends ServiceImpl<BusinessLicenseMapper, BusinessLicense> implements BusinessLicenseService {
 
 }
