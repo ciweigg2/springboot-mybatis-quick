460fe25231871f54edf23b25fb3582e8ba15709c
马秀成 <maxiucheng@transino.net>
2020-03-21 14:05:51 +0800

    提交mybatis plus多表查询例子

--- src/main/java/com/mxc/springbootmybatisquick/mybatis/mapper/DepartmentMapper.java
+++ src/main/java/com/mxc/springbootmybatisquick/mybatis/mapper/DepartmentMapper.java
@@ -21,6 +21,6 @@ public interface DepartmentMapper extends BaseMapper<Department> {
 
     //多表联合查询(没有sql注入的风险)
     @Select("select t1.*,t2.name,t2.create_time from department t1 LEFT JOIN  organization t2  ON t1.id =t2.id WHERE t1.id= #{id}")
-    List<Map<String,Object>> doList(Page<Map<String,Object>> page, String id);
+    Page<Map<String,Object>> doList(Page page, String id);
 
 }
